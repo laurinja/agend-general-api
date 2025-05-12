a1e535f1e04d9c6f49cc1511edbc9604baa19908
Laura Bareto <laurabareto@alunos.utfpr.edu.br>
2025-05-11 23:44:27 -0300

    Criação da classe participante

---
+++ Participante.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+
+namespace AgendaDeCompromissos.AgendaCompromisso
+{
+    public class Participante
+    {
+        public string NomeCompleto { get; private set; }
+
+        private List<Compromisso> _compromissos = new List<Compromisso>();
+        public IReadOnlyCollection<Compromisso> Compromissos => _compromissos.AsReadOnly();
+
+        public Participante(string nomeCompleto)
+        {
+            if (string.IsNullOrWhiteSpace(nomeCompleto))
+                throw new ArgumentException("Nome do participante é obrigatório.");
+
+            NomeCompleto = nomeCompleto;
+        }
+
+        public void AdicionarCompromisso(Compromisso compromisso)
+        {
+            if (compromisso == null)
+                throw new ArgumentNullException(nameof(compromisso));
+
+            if (!_compromissos.Contains(compromisso))
+            {
+                _compromissos.Add(compromisso);
+                compromisso.AdicionarParticipante(this);
+            }
+        }
+
+        public override string ToString()
+        {
+            return NomeCompleto;
+        }
+    }
+}
